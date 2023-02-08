8c9452ca8b61cc0bf01a5834eee8039c1bdd0fa9
presleyschumacher <presley.schumacher2@gmail.com>
2023-02-07 19:18:43 -0600

    version3

--- v3_worker.py
+++ v3_worker.py
@@ -96,4 +96,5 @@ def main(hn: str = "localhost", qn: str = "task_queue"):
 # If this is the program being run, then execute the code below
 if __name__ == "__main__":
     # call the main function with the information needed
-    main("localhost","task_queue3")
+    main("localhost", "queue_task3")
+    
