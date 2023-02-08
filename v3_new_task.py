2ec516c780b8e29b55d75168ffde10eea03e77ea
presleyschumacher <presley.schumacher2@gmail.com>
2023-02-07 19:40:57 -0600

    version3

--- v3_new_task.py
+++ v3_new_task.py
@@ -1,9 +1,9 @@
 """
-    This program sends a message to a queue on the RabbitMQ server.
+    This program sends a message to a queue on the RabbitMQ server from a CSV File.
     Make tasks harder/longer-running by adding dots at the end of the message.
 
-    Author: Denise Case
-    Date: January 15, 2023
+    Author: Presley Schumacher
+    Date: February 7, 2023
 
 """
 
@@ -13,13 +13,14 @@ import webbrowser
 import csv
 import time
 
-def offer_rabbitmq_admin_site():
-    """Offer to open the RabbitMQ Admin website"""
-    ans = input("Would you like to monitor RabbitMQ queues? y or n ")
-    print()
-    if ans.lower() == "y":
-        webbrowser.open_new("http://localhost:15672/#/queues")
+def offer_rabbitmq_admin_site(show_offer):
+    """Offer to open the RabbitMQ Admin website by using True or False"""
+    if show_offer == True:
+        ans = input("Would you like to monitor RabbitMQ queues? y or n ")
         print()
+        if ans.lower() == "y":
+            webbrowser.open_new("http://localhost:15672/#/queues")
+            print()
 
 def send_message(host: str, queue_name: str, message: str):
     """
@@ -33,17 +34,16 @@ def send_message(host: str, queue_name: str, message: str):
     """
 
     try:
-        # create a blocking connection to the RabbitMQ server
+        # Establish a connection with RabbitMQ server
         conn = pika.BlockingConnection(pika.ConnectionParameters(host))
-        # use the connection to create a communication channel
         ch = conn.channel()
         # use the channel to declare a durable queue
         # a durable queue will survive a RabbitMQ server restart
         # and help ensure messages are processed in order
         # messages will not be deleted until the consumer acknowledges
         ch.queue_declare(queue=queue_name, durable=True)
-        # use the channel to publish a message to the queue
-        # every message passes through an exchange
+        # The message goes through an exchange before being sent to the queue
+        # We will use the default exchange identified by an empty string
         ch.basic_publish(exchange="", routing_key=queue_name, body=message)
         # print a message to the console for the user
         print(f" [x] Sent {message}")
@@ -59,10 +59,16 @@ def send_message(host: str, queue_name: str, message: str):
 # without executing the code below.
 # If this is the program being run, then execute the code below
 if __name__ == "__main__":
-    offer_rabbitmq_admin_site()
+    # ask the user if they would like to open the RabbitMQ Admmin
+    offer_rabbitmq_admin_site('False')
+    # opening the csv file
     with open("tasks.csv", "r") as file:
+        # reading the csv file
         csvreader = csv.reader(file, delimiter=':')
+    # displaying the contents of the csv file
         for row in csvreader:
             task = ",".join(row)
+            # send the task to the queue
             send_message("localhost", "queue_task3", task)
-            time.sleep(3)
+            # sleep for five seconds
+            time.sleep(5)
